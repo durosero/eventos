015507d22434fa1a9b26a5ae40e0a9e4928ba9c8
Duvan Rosero <duvanrosero1995agmail.com>
2020-04-04 10:02:33 -0500

    primer evento boton rojo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("pagina cargada");
+var btnRojo = document.getElementById('btnRojo');
+var btnVerde = document.getElementById('btnVerde');
+var btnAzul = document.getElementById('btnAzul');
+var btnAmarillo = document.getElementById('btnAmarillo');
+
+var caja = document.getElementById('caja');
+
+btnRojo.onmouseover = function() {
+    console.log("Boton rojo");
+    caja.style.background = "red"
+
+}
