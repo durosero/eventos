4f7815b1f8194c689a7c7cb0a950fe6f942ba2ce
Duvan Rosero <duvanrosero1995agmail.com>
2020-04-04 10:46:33 -0500

    botones funcionando con su respectivo evento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-var btnRojo = document.getElementById('btnRojo');
-var btnVerde = document.getElementById('btnVerde');
-var btnAzul = document.getElementById('btnAzul');
-var btnAmarillo = document.getElementById('btnAmarillo');
+window.onload = function() {
 
-var caja = document.getElementById('caja');
+    var btnRojo = document.getElementById('btnRojo');
+    var btnVerde = document.getElementById('btnVerde');
+    var btnAzul = document.getElementById('btnAzul');
+    var btnAmarillo = document.getElementById('btnAmarillo');
 
-btnRojo.onmouseover = function() {
-    console.log("Boton rojo");
-    caja.style.background = "red"
+    var caja = document.getElementById('caja');
+
+    btnRojo.onmouseover = function() {
+        console.log("Boton rojo");
+        caja.style.background = "red";
+    }
+
+    btnVerde.onmouseover = function() {
+        caja.style.background = "green"
+    }
+    btnAzul.onmousemove = function() {
+        console.log("Boton Azul");
+        caja.style.background = "blue"
+    }
+    btnAmarillo.ondblclick = function() {
+        caja.style.background = "yellow"
+    }
+
+
+
+    //quitr colores 
+    btnRojo.onmouseout = quitarColor;
+    btnVerde.onmouseout = quitarColor;
+    btnAzul.onmouseout = quitarColor;
+    btnAmarillo.onmouseout = quitarColor;
+
+    function quitarColor() {
+        caja.style.background = null;
+    }
+
 
 }
